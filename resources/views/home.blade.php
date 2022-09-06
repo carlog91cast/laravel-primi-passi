0b89b511b7b8a84493eb22ca7571448326b6cc0d
bigeppo <carlog91cast@gmail.com>
2022-09-06 17:57:52 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>dopo 3 ore di smanetto sono riuscito a far funzionare laravel</h1>
+</body>
+</html>
