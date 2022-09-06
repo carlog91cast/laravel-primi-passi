23563daea2b896a2c60b45b8978519bbfc675e92
bigeppo <carlog91cast@gmail.com>
2022-09-06 18:01:33 +0200

    my greetings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>dopo 3 ore di smanetto sono riuscito a far funzionare laravel</h1>
+    <h2>un ringraziamento speciale a Luigi e Stefano per la pazienza</h2>
 </body>
 </html>
