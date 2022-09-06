413163ca4e4bd33dc053aefff64c5949e5ecfab4
bigeppo <carlog91cast@gmail.com>
2022-09-06 18:07:40 +0200

    create menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,6 @@
 <body>
     <h1>dopo 3 ore di smanetto sono riuscito a far funzionare laravel</h1>
     <h2>un ringraziamento speciale a Luigi e Stefano per la pazienza</h2>
+    <a href="/menu">guarda il menu</a>
 </body>
 </html>
